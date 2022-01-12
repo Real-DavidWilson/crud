b3e50e39f1fe6e425ad2790d17abe33d0bc26d58
real-davidwilson <davidwilsonbr2019@gmail.com>
2022-01-12 06:08:21 -0300

    refactored

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,51 +1,60 @@
 import { Router, RequestHandler } from "express";
-import { body, validationResult } from "express-validator";
+import { body, header, validationResult } from "express-validator";
 import createHttpError, { HttpError } from "http-errors";
+import jwt from "jsonwebtoken";
 
 import { ServiceError, SERVICE_ERRORS_CODE } from "../errors/serviceError";
 import { register } from "../services/user";
+import handleValidation from "../middlewares/handleValidation";
+import config from "../config";
 
 class UserRoutes {
     static register: RequestHandler = async (req, res) => {
-        try {
-            const errors = validationResult(req);
-            if (!errors.isEmpty())
-                throw createHttpError(400, {
-                    message: "Some fields did not pass validation!",
-                    errors: errors
-                        .array()
-                        .map((e) => ({ field: e.param, info: e.msg })),
-                });
-
-            const { name, email, password } = req.body;
-            const { userid, token } = await register({ name, email, password });
-
-            res.status(200).send({
-                userid,
-                name,
-                email,
-                token,
+        const errors = validationResult(req);
+        if (!errors.isEmpty())
+            throw createHttpError(400, {
+                message: "Some fields did not pass validation!",
+                errors: errors
+                    .array()
+                    .map((e) => ({ field: e.param, info: e.msg })),
             });
-        } catch (err) {
-            if (err instanceof HttpError) throw err;
-            if (
-                err instanceof ServiceError &&
-                err.code === SERVICE_ERRORS_CODE.CONFLICT
-            )
-                throw createHttpError(409, err.payload);
 
-            throw createHttpError(503, "Service unavailable, try again later!");
-        }
+        const { name, email, password } = req.body;
+        const { userid } = await register({ name, email, password });
+        const token = jwt.sign({ userid }, config.app.jwtSecret, {
+            expiresIn: "12h",
+        });
+
+        res.status(200).send({
+            userid,
+            name,
+            email,
+            token,
+        });
+    };
+
+    static update: RequestHandler = async (req, res) => {
     };
 }
 
 export default {
     use(router: Router) {
-        router.post("/register", [
+        router.post("/user/register", [
             body("name").isString().isLength({ max: 255 }).trim(),
             body("email").isEmail().normalizeEmail(),
             body("password").isLength({ min: 8 }),
+            handleValidation,
             UserRoutes.register,
+        ]);
+
+        router.put("/user", [
+            header("authorization", "Bearer token is required!").isJWT,
+            body("userid").isString(),
+            body("name").isString().isLength({ max: 255 }).trim(),
+            body("email").isEmail().normalizeEmail(),
+            body("password").isLength({ min: 8 }),
+            handleValidation,
+            UserRoutes.update,
         ]);
     },
 };
